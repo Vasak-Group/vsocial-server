db30aa7870d22e66cabb34df04ec1433c3925cc4
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-21 15:26:44 -0300

    Refactorizar módulo social para incluir servicio de Twitter y configuración

--- src/social/social.module.ts
+++ src/social/social.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SocialService } from './social.service';
 import { SocialController } from './social.controller';
+import { TwitterService } from './services/twitter.service';
 
 @Module({
   controllers: [SocialController],
-  providers: [SocialService],
+  providers: [SocialService, TwitterService, ConfigService],
 })
 export class SocialModule {}
--- src/social/social.service.ts
+++ src/social/social.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TwitterService } from './services/twitter.service';
+import { SocialNetwork } from './enums/social-network.enum';
+import { SocialNetworkManager } from './entities/social-network.abstract';
 
 @Injectable()
-export class SocialService {}
+export class SocialService {
+  constructor(
+    private readonly twitterService: TwitterService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  private getSocialNetworkManager(
+    network: SocialNetwork,
+  ): SocialNetworkManager {
+    switch (network) {
+      case SocialNetwork.TWITTER:
+        return this.twitterService;
+      default:
+        return null;
+    }
+  }
+
+  async auth(network: SocialNetwork, user: any): Promise<any> {
+    const socialNetwork = this.getSocialNetworkManager(network);
+    return socialNetwork.auth(user);
+  }
+}
