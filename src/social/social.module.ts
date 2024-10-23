1d6d3888f7193bc06090fb535b3ed15df55f1903
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-23 08:07:02 -0300

    Refactorizar módulo social para incluir servicio de Twitter y configuración

--- src/social/social.module.ts
+++ src/social/social.module.ts
@@ -3,9 +3,10 @@ import { ConfigService } from '@nestjs/config';
 import { SocialService } from './social.service';
 import { SocialController } from './social.controller';
 import { TwitterService } from './services/twitter.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  controllers: [SocialController],
+  controllers: [SocialController, HttpModule],
   providers: [SocialService, TwitterService, ConfigService],
 })
 export class SocialModule {}
