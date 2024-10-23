4e139afdcceb85d5af69815e51d6c02764771c67
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-23 08:06:04 -0300

    Refactorizar módulo social para incluir servicio de Twitter y configuración

--- src/social/entities/social-network.abstract.ts
+++ src/social/entities/social-network.abstract.ts
@@ -1,3 +1,5 @@
+import { User } from "src/user/entities/user.entity";
+
 export abstract class SocialNetworkManager {
-  abstract auth(user: any): Promise<any>;
+  abstract auth(user: User): Promise<string>;
 }
